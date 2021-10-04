c05499f6bb6f62f3aacc47e1404abb723432b512
josemramirez <josem.ramirez@gmail.com>
2021-10-04 17:33:59 -0500

    Delete PUSH_git.sh

--- PUSH_git.sh
+++
@@ -1,9 +0,0 @@
-
-###################### GIT PARAMETERS #####################################
-
-git add .
-git commit -m "Added new files v3"
-git push
-
-# josemramirez
-# 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
